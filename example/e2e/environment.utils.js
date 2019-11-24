a2f97cbd20774ff2859f753ded88c1ec997faf1b
Sebastian Pernett <pernett98@gmail.com>
2019-11-24 10:44:36 -0500

    WIP: refactor e2e tests

---
+++ example/e2e/environment.utils.js
@@ -0,0 +1,5 @@
+export const getFromEnv = (env) => (key, defaultValue) => env[key] || defaultValue || ''
+
+export const getPlatformSelect = (platform) => (platforms) => platforms[platform.toLowerCase()]
+
+export const getPlatformSelectLazy = (platformSelect) => (platforms) => platformSelect(platforms)()
--- example/e2e/index.e2e.test.js
+++ example/e2e/index.e2e.test.js
@@ -1,19 +1,28 @@
 import wd from 'wd'
+import { getFromEnv, getPlatformSelect, getPlatformSelectLazy } from './environment.utils'
+import { 
+  handleAndroidAlert,
+  handleCloseBrowserAndroid,
+  handleCloseBrowserIos,
+  handleIosAlert,
+  handleRedirectLinkAndroid,
+  handleRedirectLinkIos
+} from './appium.handlers'
 
-jasmine.DEFAULT_TIMEOUT_INTERVAL = 800000
-
-const SERVER_PORT = 4723
-const SERVER_URL = 'localhost'
-
-const getFromEnv = (env) => (key) => env[key] || ''
 const getEnv = getFromEnv(process.env)
+
+jasmine.DEFAULT_TIMEOUT_INTERVAL = getEnv('JEST_INTERVAL_TIME', 800000)
+
+const SERVER_PORT = getEnv('APPIUM_PORT', 4723)
+const SERVER_URL = getEnv('APPIUM_HOST', 'localhost')
+const WAIT_FOR_SIMULATOR = getEnv('APPIUM_WAIT_FOR_SIMULATOR', 6000)
+
 const platform = getEnv('E2E_PLATFORM')
 const platformVersion = getEnv('E2E_PLATFORM_VERSION')
 const device = getEnv('E2E_DEVICE')
 
-const platformSelect = (platforms) => {
-  return platforms[platform.toLowerCase()]
-}
+const platformSelect = getPlatformSelect(platform)
+const platformSelectLazy = getPlatformSelectLazy(platformSelect)
 
 const makeCapabilitiesIos = (deviceName, platformVersion) => ({
   platformName: 'iOS',
@@ -29,35 +38,11 @@ const makeCapabilitiesAndroid = (deviceName, platformVersion) => ({
   app: './android/app/build/outputs/apk/release/app-release.apk',
 })
 
-const sleep = (time = 500) => new Promise(resolve => setTimeout(resolve, time))
+// const sleep = (time = 500) => new Promise(resolve => setTimeout(resolve, time))
 
 const driver = wd.promiseChainRemote(SERVER_URL, SERVER_PORT)
 
-const handleIosAlert = (driver, text) => driver
-  .waitForElementByAccessibilityId(text)
-  .click()
-
-const handleAndroidAlert = (driver) => driver
-  .waitForElementById("android:id/button1")
-  .click()
-
-const handleRedirectLinkAndroid = (driver) => driver
-  .waitForElementByXPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout[1]/android.widget.FrameLayout[2]/android.webkit.WebView/android.view.View[2]")
-  .click()
-
-const handleRedirectLinkIos = (driver, text) => driver
-  .waitForElementByName(text)
-  .click()
-
-const handleCloseBrowserAndroid = (driver) => driver
-  .waitForElementByAccessibilityId("Close tab")
-  .click()
-
-const handleCloseBrowserIos = (driver, text) => driver
-  .waitForElementByAccessibilityId(text)
-  .click()
-
-describe('SomeComponent', () => {
+describe('Simple tests for deep linking and redirection', () => {
                       
   beforeAll(async () => {
     try {
@@ -65,11 +50,8 @@ describe('SomeComponent', () => {
         ios: makeCapabilitiesIos(device, platformVersion),
         android: makeCapabilitiesAndroid(device, platformVersion)
       })
-      console.log('running e2e tests with the following capabilities')
-      console.log(capabilities)
       await driver.init(capabilities)
-      await driver.sleep(250000)
-      console.log('me resolví :P ')
+      await driver.sleep(WAIT_FOR_SIMULATOR)
     } catch(err) {
       console.log(err)
     }
@@ -87,46 +69,46 @@ describe('SomeComponent', () => {
   afterAll(async () => {
     await driver.quit()
   })
-                                            
-  test('try deep linking', async () => {
-    const tryDeepBtn = await driver.waitForElementByAccessibilityId('btn_try_deep_linking')
-    await tryDeepBtn
-      .click()
-    await platformSelect({
-      ios: handleIosAlert(driver, 'Continue'),
-      android: Promise.resolve()
-    })
-    await sleep(1500)
-
-    await platformSelect({
-      ios: handleRedirectLinkIos(driver, 'Press here to redirect'),
-      android: handleRedirectLinkAndroid(driver)
-    })
-
-    await sleep(1000)
-
-     await platformSelect({
-       ios: handleIosAlert(driver, 'OK'),
-       android: handleAndroidAlert(driver)
-     }) 
-    expect(await driver.hasElementByAccessibilityId('btn_try_deep_linking')).toBe(true)
-  })  
-  
+      
   test('try redirect', async () => {
     const openLinkBtn = await driver.waitForElementByAccessibilityId('btn_open_link')
     await openLinkBtn
       .click()
-    await sleep(1000)
-    await platformSelect({
-      ios: handleCloseBrowserIos(driver, 'Cancel'),
-      android: handleCloseBrowserAndroid(driver)
+    await driver.sleep(1000)
+    await platformSelectLazy({
+      ios: () => handleCloseBrowserIos(driver, 'Cancel'),
+      android: () => handleCloseBrowserAndroid(driver)
     })
-    await sleep(1000)
-    await platformSelect({
-      ios: handleIosAlert(driver, 'OK'),
-      android: handleAndroidAlert(driver)
+    await driver.sleep(1000)
+    await platformSelectLazy({
+      ios: () => handleIosAlert(driver, 'OK'),
+      android: () => handleAndroidAlert(driver)
     })
     expect(await driver.hasElementByAccessibilityId('btn_open_link')).toBe(true)
   })
 
+  test('try deep linking', async () => {
+    const tryDeepBtn = await driver.waitForElementByAccessibilityId('btn_try_deep_linking')
+    await tryDeepBtn
+      .click()
+    await platformSelectLazy({
+      ios: () => handleIosAlert(driver, 'Continue'),
+      android: () => Promise.resolve()
+    })
+    await driver.sleep(1500)
+
+    await platformSelectLazy({
+      ios: () => handleRedirectLinkIos(driver, 'Press here to redirect'),
+      android: () => handleRedirectLinkAndroid(driver)
+    })
+
+    await driver.sleep(1000)
+
+    await platformSelectLazy({
+      ios: () => handleIosAlert(driver, 'OK'),
+      android: () => handleAndroidAlert(driver)
+    }) 
+    expect(await driver.hasElementByAccessibilityId('btn_try_deep_linking')).toBe(true)
+  })  
+  
 })
